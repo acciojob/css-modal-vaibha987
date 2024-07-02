0b1fee25ac575aa7f493e9025d18d37ce3cc7e51
vaibha987 <v2goyal95@gmail.com>
2024-07-02 19:15:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-//your JS code here. If required.
+// Get the modal, open button and close button
 var modal = document.querySelector('.modal');
-var openModalButton = document.getElementById('openModal');
+var openModalButton = document.querySelector('#openModal');
 var closeModalButton = document.querySelector('.close-modal');
+
+// When the user clicks on the button, open the modal 
 openModalButton.addEventListener('click', function() {
     modal.style.display = 'block';
+});
+
+// When the user clicks on <span> (x), close the modal
+closeModalButton.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
 });
